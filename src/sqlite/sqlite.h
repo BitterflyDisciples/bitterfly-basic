97df02c967f4349973a321bef2f21820eea5f93c
BitterflyDisciples <BitterflyDisciples@github.com>
2020-07-30 18:12:31 +0800

    initial version

---
+++ src/sqlite/sqlite.h
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------
+/*
+  This file is part of bvcd: https://github.com/BVC/BVC-core
+  Copyright (c) 2012-2015 BVC Alliance.
+  
+  Permission to use, copy, modify, and/or distribute this software for any
+  
+*/
+//==============================================================================
+
+#ifndef BVC_SQLITE_SQLITE_H_INCLUDED
+#define BVC_SQLITE_SQLITE_H_INCLUDED
+
+/**  A self-contained, serverless, zero configuration, transactional SQL engine.
+
+    This external module provides the SQLite embedded database library.
+
+    SQLite is public domain software, visit http://sqlite.org
+*/
+
+#ifndef BVC_SQLITE_CPP_INCLUDED
+#include <sqlite/sqlite/sqlite3.h>
+#endif
+
+#endif
